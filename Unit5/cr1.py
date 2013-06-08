e5afdce82d1c2919cdd1205e41452e7da7c128ba
s <s@s-virtual-machine.(none)>
2013-06-08 16:41:17 +0530

    latest version

---
+++ Unit5/cr1.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import urllib2
+import re
+def crawlh(word,word1):
+    result=[]
+    req=urllib2.Request('http://www.indianholiday.com/hotels/'+word+'-star-hotels-'+word1+'.html')
+    response=urllib2.urlopen(req)
+    msg=response.read()
+    soup = BeautifulSoup(msg)
+    ssd=re.compile("Hotel")
+    string=''
+    #string=str(soup.find('div', text=ssd))
+    for link in soup.find_all('div'):
+        if link.get('class')==['head-center']:
+                result.append( link.text.replace('Add/Read Review','').replace('\n',''))
+    return result
+def crawlt(word):
+    result=[]
+    req=urllib2.Request('http://www.makemytrip.com/travel-guides/india/'+word+'/')
+    response=urllib2.urlopen(req)
+    msg=response.read()
+    soup = BeautifulSoup(msg)
+    for link in soup.find_all('div'):
+        if link.get('id')=='dest-teaser':
+            result.append(link.text.replace('\n','').replace('[ read more ]',''))         
